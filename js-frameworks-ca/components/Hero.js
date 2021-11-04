1e1f09226f11759bdd3cfc5c2f683b7a70a2e589
Kay Alvaer <mosarwe@yahoo.co.uk>
2021-11-04 17:13:53 +0100

    Added animation to hero text

--- js-frameworks-ca/components/Hero.js
+++ js-frameworks-ca/components/Hero.js
@@ -5,11 +5,13 @@ export default function Hero() {
     return (
         <div className={styles.hero}>
             <div className={styles.heroContent}>
-                <h1>Passion To Flower Power</h1>
-                <h3>
-                    Embrace the feelings with the power of flowers. Let them 
-                    talk with you.
-                </h3>
+                <div className={styles.contentAnimate}>
+                    <h1>Passion To Flower Power</h1>
+                    <h3>
+                        Embrace the feelings with the power of flowers. Let them 
+                        talk with you.
+                    </h3>
+                </div>
                 <p className={styles.link}>
                         <Link href="pages/account/register">
                             <a className='btnHero'>Join us</a>
