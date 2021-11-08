4b20df244dd70b209dd172dda7737a44679be6db
Kay Alvaer <mosarwe@yahoo.co.uk>
2021-11-08 10:33:34 +0100

    Debug subscribe div

--- js-frameworks-ca/components/subscribe.js
+++ js-frameworks-ca/components/subscribe.js
@@ -1,21 +1,21 @@
-import styles from '@/styles/Subscribe.module.css'
-import Link from "next/link";
+// import styles from '@/styles/Subscribe.module.css'
+// import Link from "next/link";
 
-export default function Subscribe() {
-    return (
-        <div className={styles.subscribe}>
-            <h2>Our flowers seems like a great idea for you</h2>
-            <h5>
-                Our flowers seems like a great idea for you. We have a club.
-                our club member are welcome to write theoir meaning and unique 
-                experiences about flowers. Why not join them!
-            </h5>
-            <p className={styles.link}>
-                <Link href="pages/account/login">
-                            <a className='btnHero'>Join us</a>
-                </Link>
-            </p>
+// export default function Subscribe() {
+//     return (
+//         <div className={styles.subscribe}>
+//             <h2>Our flowers seems like a great idea for you</h2>
+//             <h5>
+//                 Our flowers seems like a great idea for you. We have a club.
+//                 our club member are welcome to write theoir meaning and unique 
+//                 experiences about flowers. Why not join them!
+//             </h5>
+//             <p className={styles.link}>
+//                 <Link href="pages/account/login">
+//                             <a className='btnHero'>Join us</a>
+//                 </Link>
+//             </p>
             
-        </div>
-    )
-}
+//         </div>
+//     )
+// }
